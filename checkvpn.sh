b15ff453a1e337c6ea915ac3374efdb42af24669
DoubleCai <caipiz@gmail.com>
2014-11-09 12:12:44 +0800

    ping twitter.com every checking to ensure vpn is really connected.

--- checkvpn.sh
+++ checkvpn.sh
@@ -5,32 +5,34 @@ DATE="["`date`"]"
 
 if [ -f "/tmp/vpn_status_check.lock" ]
 then
-	echo $DATE "another checker instance is running, exit" >> $LOGFILE
-        exit 0
+    echo $DATE "another checker instance is running, exit" >> $LOGFILE
+    exit 0
 fi
+
+ping -s 1024 -c 5 twitter.com
 
 VPN_CONN=`ifconfig | grep pptp-vpn`
 
 if [ -z "$VPN_CONN" ]
 then
-        touch /tmp/vpn_status_check.lock
-        echo $DATE "WAN_VPN_RECONNECT" >> $LOGFILE
+    touch /tmp/vpn_status_check.lock
+    echo $DATE "WAN_VPN_RECONNECT" >> $LOGFILE
 
-#       ifdown vpn
-#       ifdown wan
-#       sleep 10
-#       ifup wan
-#       sleep 30
+#   ifdown vpn
+#   ifdown wan
+#   sleep 10
+#   ifup wan
+#   sleep 30
 
-        ifdown vpn
-        sleep 10
-        ifup vpn
-        sleep 40
+    ifdown vpn
+    sleep 10
+    ifup vpn
+    sleep 40
 
-	sh /root/autogracevpn.sh 
+    sh /root/autogracevpn.sh
 
-        rm /tmp/vpn_status_check.lock
+    rm /tmp/vpn_status_check.lock
 else
-	echo $DATE "vpn is connected" >> $LOGFILE
+    echo $DATE "vpn is connected" >> $LOGFILE
 fi
 
