b06a72888b5e385d216922c7e07328127c5efa70
DoubleCai <caipiz@gmail.com>
2014-11-09 12:20:33 +0800

    BugFix

--- checkvpn.sh
+++ checkvpn.sh
@@ -1,21 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-LOGFILE="/tmp/autoconnvpn.log"
+
 DATE="["`date`"]"
+LOCKFILE="/tmp/checkvpn.lock"
+LOGFILE="/tmp/checkvpn.log"
+ADDROUTESHELL=$(dirname $(readlink -f $0))/addvpnroute.sh
 
-if [ -f "/tmp/vpn_status_check.lock" ]
+
+if [ -f "$LOCKFILE" ]
 then
     echo $DATE "another checker instance is running, exit" >> $LOGFILE
     exit 0
 fi
 
+
 ping -s 1024 -c 5 twitter.com
 
-VPN_CONN=`ifconfig | grep pptp-vpn`
 
+VPN_CONN=`ifconfig | grep pptp-vpn`
 if [ -z "$VPN_CONN" ]
 then
-    touch /tmp/vpn_status_check.lock
+    touch $LOCKFILE
     echo $DATE "WAN_VPN_RECONNECT" >> $LOGFILE
 
 #   ifdown vpn
@@ -29,9 +34,9 @@ then
     ifup vpn
     sleep 40
 
-    sh /root/autogracevpn.sh
+    sh $ADDROUTESHELL
 
-    rm /tmp/vpn_status_check.lock
+    rm $LOCKFILE
 else
     echo $DATE "vpn is connected" >> $LOGFILE
 fi
